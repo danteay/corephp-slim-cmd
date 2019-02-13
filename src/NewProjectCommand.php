294ab8c99fc5758df39cb02f3cfe591beb4746b4
Eduardo Aguilar Yépez <dante.aguilar41@gmail.com>
2019-02-12 20:59:07 -0600

    save

--- src/NewProjectCommand.php
+++ src/NewProjectCommand.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * New project command for Slim
+ *
+ * PHP Version 7.1
+ *
+ * @category  Command
+ * @package   CorePHP_Slim_Cmd
+ * @author    Eduardo Aguilar <dante.aguilar41@gmail.com>
+ * @copyright 2019 Eduardo Aguilar
+ * @license   https://github.com/danteay/corephp-slim-cmd/LICENSE Apache-2.0
+ * @link      https://github.com/danteay/corephp-slim-cmd
+ */
 
 namespace CorePHP\Slim\Console;
 
@@ -8,8 +20,23 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
+/**
+ * NewProjectCommand
+ *
+ * @category  Class
+ * @package   CorePHP_Slim_Cmd
+ * @author    Eduardo Aguilar <dante.aguilar41@gmail.com>
+ * @copyright 2019 Eduardo Aguilar
+ * @license   https://github.com/danteay/corephp-slim-cmd/LICENSE Apache-2.0
+ * @link      https://github.com/danteay/corephp-slim-cmd
+ */
 class NewProjectCommand extends Command
 {
+    /**
+     * Command name
+     *
+     * @var string
+     */
     protected static $defaultName = 'app:init';
 
     /**
@@ -20,20 +47,24 @@ class NewProjectCommand extends Command
     protected function configure()
     {
         $this->setDescription('Create a new Slim project')
-            ->addArgument('projectdir', InputArgument::REQUIRED, 'The username of the user.');
+            ->addArgument(
+                'projectdir',
+                InputArgument::REQUIRED,
+                'The username of the user.'
+            );
     }
 
     /**
      * Execute the command.
      *
-     * @param  InputInterface  $input
-     * @param  OutputInterface  $output
+     * @param InputInterface  $input  Input of the command
+     * @param OutputInterface $output Output for the command line
      *
      * @return void
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $directory = getcwd().'/'.$input->getArgument('projectdir');
+        $directory = getcwd() . '/' . $input->getArgument('projectdir');
         $output->writeln('<info>Creating project: '. $directory . '</info>');
 
         $this->verifyApplicationDoesntExist($directory);
@@ -48,9 +79,11 @@ class NewProjectCommand extends Command
         ];
 
         $process = new Process($commands, null, null, null, null);
-        $process->run(function ($type, $line) use ($output) {
-            $output->write($line);
-        });
+        $process->run(
+            function ($type, $line) use ($output) {
+                $output->write($line);
+            }
+        );
 
         $output->writeln('<comment>Project is allready installed!!</comment>');
     }
@@ -62,24 +95,24 @@ class NewProjectCommand extends Command
      */
     protected function findComposer()
     {
-        if (file_exists(getcwd().'/composer.phar')) {
-
-            return '"'.PHP_BINARY.'" composer.phar';
+        if (file_exists(getcwd() . '/composer.phar')) {
+            return '"' . PHP_BINARY . '" composer.phar';
         }
+
         return 'composer';
     }
 
     /**
      * Verify that the application does not already exist.
      *
-     * @param  string $directory
+     * @param string $directory Directory path to evaluate
      *
      * @return void
      */
     protected function verifyApplicationDoesntExist($directory)
     {
         if (is_dir($directory)) {
-            throw new \RuntimeException('Application already exists: ' . $directory);
+            throw new \RuntimeException('Application already exists: '.$directory);
         }
     }
 }
